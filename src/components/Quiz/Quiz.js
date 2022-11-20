06fcd6ac39ae8a56bdbd7952ac872d6cf9329234
mizbauddintareqe <mizbauddintareq@gmail.com>
2022-11-20 23:46:10 +0600

    added eye icon for correct answer

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Swal from "sweetalert2";
+import { EyeIcon } from "@heroicons/react/24/solid";
+
 const Quiz = ({ quiz }) => {
   const handleAnswer = (ans) => {
-    if (ans === quiz.correctAnswer) {
+    if (ans === quiz?.correctAnswer) {
       Swal.fire({
         position: "top-center",
         icon: "success",
@@ -17,6 +19,13 @@ const Quiz = ({ quiz }) => {
       });
     }
   };
+
+  const handleRightAnswer = (ans) => {
+    Swal.fire({
+      text: `${ans}`,
+    });
+  };
+
   return (
     <div>
       <div
@@ -26,11 +35,16 @@ const Quiz = ({ quiz }) => {
         <div className="collapse-title text-xl font-medium">
           {quiz?.question}
         </div>
-        <div className="collapse-content text-center">
+        <div className="flex justify-end mr-3">
+          <button onClick={() => handleRightAnswer(quiz.correctAnswer)}>
+            <EyeIcon className="h-6 w-6" />
+          </button>
+        </div>
+        <div className="collapse-content ">
           {quiz?.options?.map((option, idx) => (
             <div
               key={idx}
-              className="my-10 cursor-pointer"
+              className="my-10 cursor-pointer text-center"
               onClick={() => handleAnswer(option)}
             >
               <span className="text-xl font-bold border-cyan-500 border-2 px-5 py-2">
